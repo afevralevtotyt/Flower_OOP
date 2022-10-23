bfc341b7a174ca6b6e6f34fc4a6331e072b3bac8
afevralevtotyt <afevraleff@ya.ru>
2022-10-23 16:42:53 +0200

    done

---
+++ src/Flower.java
@@ -0,0 +1,92 @@
+import java.util.Date;
+
+public class Flower {
+    private String flowerColor;
+    private String country;
+    private double cost;
+    int lifeSpan;
+
+    private String name;
+    public Date birthday = new Date(2001, 9, 8);
+
+    public Flower(String name, String flowerColor, String country, double cost, int lifeSpan) {
+        if (name!=null&&!name.isEmpty()&&!name.isBlank()){
+            this.name = name;}
+        else {
+            this.name = "Цветок";
+        }
+        if (flowerColor!=null&&!flowerColor.isEmpty()&&!flowerColor.isBlank()){
+            this.flowerColor = flowerColor;}
+        else {
+            this.flowerColor = "белый";
+        }
+        if (country!=null&&!country.isEmpty()&&!country.isBlank()){
+            this.country = country;}
+        else {
+            this.country = "Россия";
+        }
+        if (cost>0){
+            this.cost = cost;}
+        else{
+            this.cost=1;
+        }
+        if (lifeSpan>0){
+            this.lifeSpan = lifeSpan;}
+        else{
+            this.lifeSpan = 3;
+        }
+    }
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public String getFlowerColor() {
+        return flowerColor;
+    }
+
+    public void setFlowerColor(String flowerColor) {
+        if (flowerColor!=null&&!flowerColor.isEmpty()&&!flowerColor.isBlank()){
+            this.flowerColor = flowerColor;}
+        else {
+            this.flowerColor = "белый";
+        }
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        if (country!=null&&!country.isEmpty()&&!country.isBlank()){
+            this.country = country;}
+        else {
+            this.country = "Россия";
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        if (cost>0){
+            this.cost = cost;}
+        else{
+            this.cost=1;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        String formattedDouble = String.format("%.2f", cost);
+        return  name +
+                ", цвет " + flowerColor +  ", страна " + country +
+                ", стоимость " + formattedDouble+ " рублей"+
+                ", время стояния " + lifeSpan + " суток";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.time.LocalDate;
+import java.time.temporal.TemporalAmount;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import static java.util.Collections.min;
+
+import java.util.*;
+
+import static java.util.Collections.min;
+
 public class Main {
+
+    public static void flowersCalc(Map<Flower, Integer> collec) {
+        double sum = 0;
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Flower, Integer> entry : collec.entrySet()) {
+            sum += entry.getKey().getCost() * entry.getValue();
+            list.add(entry.getKey().getLifeSpan());
+            System.out.printf("Букет будет стоить %.2f и простоит %d суток", (sum + sum * 0.1), min(list));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Flower rose = new Flower("Роза обыкновенная", "", "Голландия", 35.59, 0);
+        Flower chrysanthemum = new Flower("Хризантема", "", "", 15, 5);
+        Flower peony = new Flower("Пион", "", "Англия", 69.9, 1);
+        Flower gypsophila = new Flower("Гипсофила", "", "Турция", 19.5, 10);
+        System.out.println(rose);
+
+
+        Map<Flower, Integer> collect = new HashMap<>();
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите название цветка для букета. Доступные цветы Роза, Хризантема, Пион, Гипсофила.\nИли введите стоп для формирования букета ");
+            String i = in.nextLine();
+            i = i.toLowerCase();
+            if (i.equals("стоп")) {
+                in.close();
+                break;
+            }
+            System.out.print("Введите количество цветов ");
+            int quant = in.nextInt();
+            if (i.equals("роза")) {
+                collect.put(rose, quant);
+            } else if (i.equals("хризантема")) {
+                collect.put(chrysanthemum, quant);
+            } else if (i.equals("пион")) {
+                collect.put(peony, quant);
+            } else if (i.equals("гипсофила")) {
+                collect.put(gypsophila, quant);
+            }
+        }
+        flowersCalc(collect);
+        System.out.println(rose);
     }
 }
